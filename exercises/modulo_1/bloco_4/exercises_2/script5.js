6213a784168e9e800f3f2bea2a5a2d9e13a0e1aa
Wesley de Melo Pedroso <wesleyxd@hotmail.com>
2021-05-19 18:40:08 -0300

    5- Triângulo

---
+++ exercises/modulo_1/bloco_4/exercises_2/script5.js
@@ -0,0 +1,12 @@
+let angulo1 = 10;
+let angulo2 = 20;
+let angulo3 = 160;
+
+let somaAngulo = angulo1 + angulo2 + angulo3;
+
+if (somaAngulo == 180) {
+  console.log(true);
+}else {
+  console.log(false);
+  console.log("A soma de todos os ângulos deve ser 180 para ser um triângulo.");
+}
