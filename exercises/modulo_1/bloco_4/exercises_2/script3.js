6b3f1ff564739fb70527481a9782aa7de0d2e9f4
Wesley de Melo Pedroso <wesleyxd@hotmail.com>
2021-05-19 18:23:40 -0300

    3- Retornar o número maior de 3 variáveis

---
+++ exercises/modulo_1/bloco_4/exercises_2/script3.js
@@ -0,0 +1,11 @@
+let number1 = 4;
+let number2 = 6;
+let number3 = 5;
+
+if (number1 > number2) {
+  console.log(number1);
+} else if (number2 > number3) {
+  console.log(number2);
+}else {
+  console.log(number3);
+}
