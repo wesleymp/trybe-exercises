022e3151bc23e1e8913fbf0a6e88e208da5b47ed
Wesley de Melo Pedroso <wesleyxd@hotmail.com>
2021-05-19 15:58:59 -0300

    Conteúdo Bloco 4 - dia 1 - parte 3

---
+++ exercises/modulo_1/bloco_4/exercises_1/script2.js
@@ -0,0 +1,25 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log("Tipo das Váriaveis:");
+console.log( typeof patientId);
+console.log( typeof isEnrolled);
+console.log( typeof patientInfo);
+console.log( typeof patientEmail);
+console.log( typeof patientAge);
+
+console.log("");
+
+console.log("Operadores:");
+let base = 5;
+let altura = 8;
+let area = base * altura;
+let perimetro = base + base + altura + altura;
+
+console.log(area);
+console.log(perimetro);
